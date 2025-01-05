b924ac403f8419fec1307506ba15820a3328936b
akshatasingh1 <singakshata1203@gmail.com>
2025-01-05 21:47:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from transformers import pipeline
 
 text_summary = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", torch_dtype=torch.bfloat16)
 
-# model_path = ("../Models/models--sshleifer--distilbart-cnn-12-6/snapshots"
-#               "/a4f8f3ea906ed274767e9906dbaede7531d660ff")
-# text_summary = pipeline("summarization", model=model_path,
-#                 torch_dtype=torch.bfloat16)
+model_path = ("../Models/models--sshleifer--distilbart-cnn-12-6/snapshots"
+               "/a4f8f3ea906ed274767e9906dbaede7531d660ff")
+ text_summary = pipeline("summarization", model=model_path,
+                torch_dtype=torch.bfloat16)
 
 
 
